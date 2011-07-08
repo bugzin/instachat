e8f4a44a387f1d47e6266423ee2499e04fd48a5a
bugzu <bugzuu@gmail.com>
2011-07-08 22:32:19 +0530

    checking

--- server.js
+++ server.js
@@ -7,10 +7,20 @@ var app  = express.createServer(function(req, res){
 	res.end("Chat Room using socke");
 });
 
+app.configure(function(){
+	app.use('view engine','jade');
+	app.use(express.static(__dirname + '/client'));
+	app.use(express.bodyParser());
+});
 
+app.get('/',function(req,res){
+	res.render('index.jade',{title:'Chat Room'}
+});
+    )
+var io = require('socket.io').listen(app);
 
-
-
-
+io.sockets.on('connection',function(socket){
+	console.log('Connected');
+});
 
 app.listen(8005);
