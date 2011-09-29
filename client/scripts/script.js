c51f3fa364f910f1fe27c4aac9d9842b1460ee36
Gaurav Sharma <Gaurav@Gaurav-Sharmas-iMac.local>
2011-09-29 21:26:58 +0530

    Rooms Layout

--- client/scripts/script.js
+++ client/scripts/script.js
@@ -1,3 +1,31 @@
 (function($){
-    alert($);
+	var socket = io.connect();
+	socket.on('connect', function(){
+		
+		socket.on('msg',function(msg){
+			alert(msg)
+		});
+		
+		socket.on('disconnect', function(){
+			
+		});
+	});
+	
+	$(function(){
+		$('#login .button').click(function(){
+			var handle = $('#handle').val();
+			if(handle == ""){
+				alert('Please enter nick name!')
+			}else{
+				var room = this.innerText;
+				location.href = "/room";
+				socket.emit('join',{room: room, handle: handle});
+			}
+		});
+		
+		$("#send").click(function(){
+			var msg = $('textarea').val();
+			socket.emit('msg',{msg: msg});
+		});
+	});
 })(jQuery);
--- server.js
+++ server.js
@@ -3,9 +3,7 @@ var http = require('http'),
 
 var express = require('express');
 
-var app  = express.createServer(function(req, res){
-	res.end("Chat Room using socke");
-});
+var app  = express.createServer();
 
 app.configure(function(){
 	app.use('view engine','jade');
@@ -14,13 +12,27 @@ app.configure(function(){
 });
 
 app.get('/',function(req,res){
-	res.render('index.jade',{title:'Chat Room'}
+	res.render('index.jade',{title:'Chat Room'});
 });
-    )
+
+app.get('/room',function(req,res){
+	res.render('room.jade',{title:'Chat Room'});
+});
+    
 var io = require('socket.io').listen(app);
+io.set('log level', 1);
 
 io.sockets.on('connection',function(socket){
 	console.log('Connected');
+	socket.on('join', function(name){
+		socket.join(name.room);
+		var info = JSON.stringify(name);
+		socket.set('nickname', info, function () { socket.emit('ready'); });
+	});
+	
+	socket.on('msg', function(msg){
+		console.log(JSON.stringify(msg))
+	});
 });
 
 app.listen(8005);
